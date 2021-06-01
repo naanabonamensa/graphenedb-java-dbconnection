6710d619338accb57ebc8498c0f2abc45875ec7e
naanabonamensa <85186082+naanabonamensa@users.noreply.github.com>
2021-06-01 19:11:30 +0100

    Update conn.java

--- conn.java
+++ conn.java
@@ -7,13 +7,26 @@ public class App
     Config.ConfigBuilder builder = Config.builder().withEncryption();
     Config config = builder.build();
 
-    Driver driver = GraphDatabase.driver("YOUR DATABASE URL", AuthTokens.basic( "YOUR DB USERNAME", "YOUR DB PASSWORD" ), config);
+    Driver driver = GraphDatabase.driver("YOUR DATABASE URL", AuthTokens.basic( "YOUR NEW DB USERNAME", "YOUR NEW DB PASSWORD" ), config);
+    Driver driver2 = GraphDatabase.driver("YOUR DATABASE URL", AuthTokens.basic( "YOUR OLD DB USERNAME", "YOUR OLD DB PASSWORD" ), config);
 
+    
     Session session = driver.session();
+    Session session2 = driver2.session();
 
     Result result = session.run("SOME UNIQUE QUERY");
+    Result result2 = session2.run("SOME UNIQUE QUERY");
+    
+    if (result === result2) {
+      System.out.println("The two databases are the same");
+    } else {
+      System.out.println("The two databases are not the same");
+    }
+
 
     session.close();
+    session2.close();
     driver.close();
+    driver2.close();
   }
 }
